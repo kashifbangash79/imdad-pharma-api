85a4cafdabaad5b690df61a7d3c3d91253210be8
Kashif Ur Rehman <88799658+kashifbangash79@users.noreply.github.com>
2024-08-17 03:42:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,25 @@ const app = express();
 app.use(bodyParser.json());
 
 const corsOptions = {
-    origin: ["https://imdad-pharma.vercel.app","http://localhost:3000"],
+    origin: 'https://imdad-pharma.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
-
-// Apply CORS middleware
 app.use(cors(corsOptions));
 app.use(express.json());
+
+// Debugging middleware to log requests and responses
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://imdad-pharma.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    console.log(`Request headers:`, req.headers);
+    res.on('finish', () => {
+        console.log(`Response headers:`, res.getHeaders());
+    });
+    next();
+});
 
 const PurchaseHistoryRoutes = require('./routes/auth');
 const agentPaymentsRoutes = require('./routes/agentPayments');
