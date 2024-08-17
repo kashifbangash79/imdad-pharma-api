983dea3f53aeff1ba2a2c87b934f6b713bee4dce
Kashif Ur Rehman <88799658+kashifbangash79@users.noreply.github.com>
2024-08-17 13:51:09 +0500

    updated commit for cors

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
-// const corsOptions = {
-//   origin: ['https://imdad-pharma.vercel.app','*'],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   optionsSuccessStatus: 204
-// };
-
 // Apply CORS middleware
-app.use(cors());
+const corsOptions = {
+  origin: ['https://imdad-pharma.vercel.app', '*'],  // Your frontend URL and allow any origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
